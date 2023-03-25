e1c9f5721ba319f63814161ac2dd9485bdc55926
Filip Szumski <filip.szumski@carnext.com>
2023-03-25 17:30:58 +0100

    feat: optimize images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["fakestoreapi.com", "picsum.photos"],
+    formats: ["image/avif", "image/webp"],
+  },
+
   async redirects() {
     return [
       {
--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import { ProductsApiResponse } from "@/api/products/types";
+import Image from "next/image";
 
 type ProductDetailsProps = {
   data: ProductsApiResponse;
@@ -9,8 +10,8 @@ export const ProductDetails = ({
 }: ProductDetailsProps) => {
   return (
     <div className="bg-white rounded-xl shadow-xl grid grid-cols-1 md:grid-cols-2 p-6 gap-6">
-      <div className="min-w-full aspect-square">
-        <img src={image} alt={title} className="w-full h-full object-contain" />
+      <div className="relative aspect-video">
+        <Image src={image} alt={title} fill className="object-contain" />
       </div>
       <div className="flex flex-col justify-start gap-4">
         <h2 className="text-2xl">{title}</h2>
