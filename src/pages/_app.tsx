a1bb4519bf83d51c7a5c4e55b281e37c86187a8e
Filip Szumski <filip.szumski@carnext.com>
2023-03-31 20:32:17 +0200

    feat: default seo config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { Layout } from "@/components";
 import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import SEO from "../../next-seo.config";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <DefaultSeo {...SEO} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
