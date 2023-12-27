2ca9dc82eded7785f92ae2b45fe93f4932aaf6ad
Filip Szumski <filipszumski@gmail.com>
2023-12-27 20:27:25 +0100

    feat: env variable for api url

--- src/api/products/getProduct.ts
+++ src/api/products/getProduct.ts
@@ -1,11 +1,8 @@
-import axios from "axios";
-
+import axios from "../apiClient";
 import { Product } from "./types";
 
 export const getProduct = async (id: string) => {
-  const response = await axios.get<Product>(
-    `https://naszsklep-api.vercel.app/api/products/${id}`,
-  );
+  const response = await axios.get<Product>(`/products/${id}`);
 
   return response.data;
 };
