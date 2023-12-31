cdcb7697fa6f555aa8bf55714823f6d13329ed4a
Filip Szumski <filipszumski@gmail.com>
2023-12-31 04:27:02 +0100

    fix: seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,5 +1,7 @@
 import { DefaultSeoProps } from "next-seo";
 
+import logo from "./public/everyware-logo-og.svg";
+
 const title = "Everyware - Online Shop";
 const description =
   "Find everything you need in one click! Our online shop offers a wide variety of items, ensuring a hassle-free and enjoyable shopping experience. Dive into the world of endless choices today";
@@ -12,6 +14,13 @@ const nextSeoConfig: DefaultSeoProps = {
     title,
     description,
     url: process.env.APP_URL,
+    images: [
+      {
+        url: `${process.env.APP_URL}${logo.src}`,
+        alt: "Everyware logo",
+        type: "image/jpeg",
+      },
+    ],
     siteName: title,
   },
 };
