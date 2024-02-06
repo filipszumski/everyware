e709a5bb7357e56958b48d240be3d7e173c973e1
Filip Szumski <filipszumski@gmail.com>
2024-02-06 22:42:54 +0100

    feat: manage cart state in local sotrage

---
+++ src/context/cartContext/types.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+import { CartItemDataSchema, CartItemSchema } from "./schemas";
+
+export type CartItemData = z.infer<typeof CartItemDataSchema>;
+
+export type CartItem = z.infer<typeof CartItemSchema>;
+
+export type CartState = {
+  cartItems: CartItem[];
+  addItemToCart: (cartItemData: CartItemData) => void;
+  removeItemFromCart: (id: CartItemData["id"]) => void;
+  clearCart: () => void;
+  allCartItemsQuantity: number;
+  updateCartItemQuantity: (
+    id: CartItemData["id"],
+    qty: CartItem["quantity"],
+  ) => void;
+};
